ca38cb0ecf23e272b74979df909d9efde89b89ab
kevin.herbas <kevin.herbas@infoleads.org>
2023-04-05 17:31:13 +0200

    Initial commit

---
+++ test/appsync-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncApi from '../lib/appsync-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncApi.AppsyncApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
